9a8dacf645a928983ce6ca166af27b3301ec3182
Brayan Ceron <brayan0np@gmail.com>
2023-07-13 22:29:13 -0500

    funciones Masa

--- conversor.js
+++ conversor.js
@@ -25,4 +25,13 @@ function Pastobar(value) {
     r = (value / 100000).toFixed(7);
     return r;
 }
-
+//Kg to Gr
+function KgtoGr(value) {
+    r = (1000 * value).toFixed(2);
+    return r;
+}
+//Gr to Kg
+function GrtoKg(value) {
+    r = (value / 1000).toFixed(4);
+    return r;
+}
